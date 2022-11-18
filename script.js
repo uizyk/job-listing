0263ce4a027734dfded8ab50251c9760482ce2ed
Sam <106921485+uizyk@users.noreply.github.com>
2022-11-18 14:57:14 +0900

    add new and featured

--- script.js
+++ script.js
@@ -19,17 +19,21 @@ req.onload = function(){
         card = "";
         for(let i=0; i<data.length; i++){
 
-
-            if(data[i].languages.length > 0){
-                let isNew = 'NEW!'
-            } else { isNew = ''}
+            let isNew =''
+            if(data[i].new === true){
+                isNew = 'NEW!'
+            } 
+            let isFeatured = ''
+            if(data[i].featured === true){
+                isFeatured = 'FEATURED'
+            }
 
             card += `
             
                 <div class="card">
                     <img src="${data[i].logo}"/>
                     <div class="card-center">
-                        <p>${data[i].company}</p>    
+                        <p>${data[i].company} ${isNew} ${isFeatured}</p>    
                         <h3>${data[i].position}</h3>
                         <p>${data[i].postedAt} • ${data[i].contract} • ${data[i].location}</p>
                     </div>
